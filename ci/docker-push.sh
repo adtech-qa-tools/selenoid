6fc9055e99d9abc1011c70f058025b99dff19ecc
s.lyboslavskiy <s.lyboslavskiy@propellerads.net>
2025-09-23 12:20:06 +0200

    Update docker-push.sh to change Docker Hub repository to adtechqatools

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-DOCKERHUB_REPOSITORY=qapropeller/selenoid
+DOCKERHUB_REPOSITORY=adtechqatools/selenoid
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker buildx build --pull --push -t "$DOCKERHUB_REPOSITORY:$1" --platform linux/amd64,linux/arm64 .
